d0e9880bce6734cab3d8469836db8da4d6dc45ca
Muhammadhasnain <webdevhasnain@gmail.com>
2026-04-23 16:28:27 +0500

    Added quiz questions dat

---
+++ app.js
@@ -0,0 +1,77 @@
+let btn = document.querySelector(".next-btn");
+
+
+
+const quizData = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "High Text Machine Language",
+      "Hyper Transfer Markup Language",
+      "Home Tool Markup Language"
+    ],
+    correct: "Hyper Text Markup Language"
+  },
+  {
+    question: "Which tag is used for JavaScript?",
+    options: [
+      "<script>",
+      "<js>",
+      "<javascript>",
+      "<code>"
+    ],
+    correct: "<script>"
+  },
+  {
+    question: "Which CSS property changes text color?",
+    options: [
+      "font-color",
+      "text-color",
+      "color",
+      "fgcolor"
+    ],
+    correct: "color"
+  },
+  {
+    question: "Which company created JavaScript?",
+    options: [
+      "Microsoft",
+      "Netscape",
+      "Google",
+      "Apple"
+    ],
+    correct: "Netscape"
+  },
+  {
+    question: "Which keyword is used to declare a variable?",
+    options: [
+      "var",
+      "let",
+      "const",
+      "All of the above"
+    ],
+    correct: "All of the above"
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: [
+      "//",
+      "<!-- -->",
+      "#",
+      "**"
+    ],
+    correct: "//"
+  },
+  {
+    question: "Which method is used to print in console?",
+    options: [
+      "print()",
+      "console.log()",
+      "log.console()",
+      "show()"
+    ],
+    correct: "console.log()"
+  }
+];
+
