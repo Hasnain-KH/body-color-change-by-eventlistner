540f4bbfc89c36687c71106157cd387f868a801d
Muhammadhasnain <webdevhasnain@gmail.com>
2026-04-23 15:36:25 +0500

    update

--- app.js
+++ app.js
@@ -9,9 +9,14 @@
             body.style.backgroundColor = "black";
             body.style.color = "white";
             h1.style.textShadow = "2px 2px 2px white";
+            h1.innerHTML = `"Dark mode is activated."`;
+            btn.textContent = "DarkMode"
+            
         } else {
             body.style.backgroundColor = "white"
             body.style.color = "black";
             h1.style.textShadow = "1px 1px 1px black";
+            h1.innerHTML = `"Light mode is activated."`;
+            btn.textContent = "WhiteMode";
         }
     })
